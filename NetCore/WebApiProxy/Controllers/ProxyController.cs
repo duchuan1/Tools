4fe46392a4c60d94e35a550e8dd37723064d8a55
cd <du498538346@outlook.com>
2020-04-26 17:17:00 +0800

    add wepapi proxy

---
+++ NetCore/WebApiProxy/Controllers/ProxyController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using log4net;
+using Newtonsoft.Json;
+
+namespace WebApiProxy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProxyController : ControllerBase
+    {
+        IHttpClientFactory _httpClientFactory = null;
+        ILog _logger = null;
+        public ProxyController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = LogManager.GetLogger(Startup.repository.Name, typeof(ProxyController));
+        }
+
+        [HttpGet("test")]
+        public string Test(string test)
+        {
+            try
+            {
+                _logger.Info($"Get {test}");
+                return test;
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Get Error： {ex.Message}");
+                throw ex;
+            }
+        }
+
+        [HttpGet]
+        public ContentResult Get(string url)
+        {
+            try
+            {
+                _logger.Info($"Get Req Url: {url}");
+                _logger.Info($"Get Req Headers: {JsonConvert.SerializeObject(Request.Headers)}");
+
+                HttpClient httpClient = _httpClientFactory.CreateClient();
+                httpClient.DefaultRequestHeaders.Clear();
+                foreach (var header in Request.Headers)
+                {
+                    httpClient.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value.ToString());
+                }
+                HttpResponseMessage httpResponseMessage =  httpClient.GetAsync(url).GetAwaiter().GetResult();
+                string sContent = httpResponseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                _logger.Info($"Get Rsp Content: {sContent}");
+
+                return GetBody(sContent);
+            }
+            catch(Exception ex)
+            {
+                _logger.Error($"Get【{url}】Error： {ex.Message}");
+                throw ex;
+            }
+        }
+
+        [HttpPost]
+        public  ContentResult Post(string url)
+        {
+            try
+            {
+                _logger.Info($"Post Req Url: {url}");
+                _logger.Info($"Post Req Headers: {JsonConvert.SerializeObject(Request.Headers)}");
+
+                var httpClient = _httpClientFactory.CreateClient();
+                httpClient.DefaultRequestHeaders.Clear();
+                foreach (var header in Request.HttpContext.Request.Headers)
+                {
+                    httpClient.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value.ToString());
+                }
+
+                //HttpContext httpContext = new StreamContent(Request.Body); 
+                string sBody = new StreamReader(Request.Body).ReadToEndAsync().GetAwaiter().GetResult();
+                _logger.Info($"Post Req Body: {sBody}");
+
+                StringContent content = new StringContent(sBody);
+                content.Headers.Clear();
+                //StreamContent content = new StreamContent(Request.Body);
+
+                foreach (var header in Request.Headers)
+                {
+                    content.Headers.TryAddWithoutValidation(header.Key, header.Value.ToString());
+                }
+
+                HttpResponseMessage httpResponseMessage = httpClient.PostAsync(url, content).GetAwaiter().GetResult();
+
+                string sContent = httpResponseMessage.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                _logger.Info($"Post Rsp Headers: {JsonConvert.SerializeObject(httpResponseMessage.Headers)}");
+                _logger.Info($"Post Rsp Content: {sContent}");
+
+                return GetBody(sContent) ;
+            }
+            catch(Exception ex)
+            {
+                _logger.Error($"Post【{url}】Error： {ex.Message}");
+                throw ex;
+
+            }
+        }
+        ContentResult GetBody(string sSontent)
+        {
+            var content = new ContentResult();
+            content.Content = sSontent;
+            content.ContentType = "application/json;charset=utf-8";
+
+            return content;
+        }
+        //public async Task<HttpResponseMessage> Put(string url)
+        //{
+        //}
+
+        //public async Task<HttpResponseMessage> Delete(string url)
+        //{
+        //}
+    }
+}
---
+++ NetCore/WebApiProxy/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
+using log4net.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WebApiProxy
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            repository = LogManager.CreateRepository("NETCoreRepository");
+            // 指定配置文件
+            XmlConfigurator.Configure(repository, new FileInfo("log4net.config"));
+        }
+
+        public IConfiguration Configuration { get; }
+        public static ILoggerRepository repository { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
